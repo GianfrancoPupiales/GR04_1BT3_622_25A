eedad75fee53acbef9f0048ad5509331e9f0294e
Gianfranco Nicolás <gianfrancopr08@gmail.com>
2025-04-22 20:47:42 -0500

    Add UserService

---
+++ src/main/java/model/service/UserService.java
@@ -0,0 +1,24 @@
+package model.service;
+
+import model.entities.User;
+import model.dao.UserDAO;
+
+public class UserService {
+    private final UserDAO userDAO;
+
+    public UserService() {
+        userDAO = new UserDAO();
+    }
+
+    public User authenticate(String dni, String password) {
+        User user = userDAO.findUserByDni(dni);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
+    }
+
+    public boolean createUser(User user) {
+        return userDAO.create(user);
+    }
+}

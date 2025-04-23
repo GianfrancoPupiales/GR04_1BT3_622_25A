54e908e38726bdcc18c436f8b6ea9bf00bfc6bf6
Gianfranco Nicolás <gianfrancopr08@gmail.com>
2025-04-22 20:48:04 -0500

    Add Product

---
+++ src/main/java/model/entities/Product.java
@@ -0,0 +1,121 @@
+package model.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Entity
+public class Product implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idProduct")
+    private int idProduct;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name ="datePublication")
+    private Date datePublication;
+
+    @Column(name = "isAvailable")
+    private boolean isAvailable;
+
+    @ManyToOne
+    @JoinColumn(name = "idUser")
+    private User user;
+
+    public Product() {
+    }
+
+    public Product(int idProduct, String title, String description, String state, User user) {
+        this.idProduct = idProduct;
+        this.title = title;
+        this.description = description;
+        this.state = state;
+        this.datePublication = new Date();
+        this.isAvailable = true;
+        this.user = user;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Date getDatePublication() {
+        return datePublication;
+    }
+
+    public void setDatePublication(Date datePublication) {
+        this.datePublication = datePublication;
+    }
+
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}

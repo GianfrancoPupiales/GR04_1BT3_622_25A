5ebf38e566133f68245db1ff74794db4d29164e2
JonathanA203 <jonathan.angulo@epn.edu.ec>
2025-05-17 19:58:37 -0500

    Dev(JonathanA): The service for Profile has been created

--- src/main/java/model/service/ProfileService.java
+++ src/main/java/model/service/ProfileService.java
@@ -8,4 +8,50 @@ import java.util.List;
 
 public class ProfileService {
 
+    private final ProfileDAO profileDAO;
+
+    public ProfileService(ProfileDAO profileDAO) {
+        this.profileDAO = profileDAO;
+    }
+
+    public ProfileService() {
+        profileDAO = new ProfileDAO();
+    }
+
+    public Profile getProfileByUserId(int userId) {
+        return profileDAO.findProfileByUserId(userId);
+    }
+
+    public boolean updateProfile(Profile profile) {
+        if (!validateProfile(profile)) {
+            return false;
+        }
+        return profileDAO.update(profile);
+    }
+
+    public boolean validateProfile(Profile profile) {
+        return profile != null
+                && profile.getFirstName() != null && !profile.getFirstName().isBlank()
+                && profile.getLastName() != null && !profile.getLastName().isBlank()
+                && profile.getPhoto() != null && !profile.getPhoto().isBlank();
+    }
+
+    /*
+    public Profile getFullProfileWithProducts(int userId) {
+        Profile profile = profileDAO.findByUserId(userId);
+        if (profile != null) {
+            List<Product> userProducts = profileDAO.findProductsByUserId(userId);
+            profile.setProducts(userProducts);
+        }
+        return profile;
+    }
+    */
+
+    public boolean createProfile(Profile profile) {
+        return profileDAO.create(profile);
+    }
+
+    public Profile getFullProfileWithProducts(int i) {
+        return null;
+    }
 }

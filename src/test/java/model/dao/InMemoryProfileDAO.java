cac1ef7ef1c79c27e0917c3372147988ed12d96b
Gianfranco Nicolás <gianfrancopr08@gmail.com>
2025-05-18 14:59:13 -0500

    feat(Gian P): add methods for saving users and products

--- src/test/java/model/dao/InMemoryProfileDAO.java
+++ src/test/java/model/dao/InMemoryProfileDAO.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import model.entities.Profile;
 import model.entities.Product;
+import model.entities.User;
 
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +12,7 @@ public class InMemoryProfileDAO extends ProfileDAO {
 
     public final Map<Integer, Profile> store = new HashMap<>();
     private final Map<Integer, List<Product>> productsStore = new HashMap<>();
+    private final Map<Integer, User> usersStore = new HashMap<>();
 
     @Override
     public Profile findProfileByUserId(int userId) {
@@ -18,26 +20,32 @@ public class InMemoryProfileDAO extends ProfileDAO {
     }
 
     public void save(Profile profile) {
-        store.put(profile.getId(), profile);
+        store.put(profile.getIdProfile(), profile);
     }
 
     @Override
     public boolean update(Profile profile) {
-        if (!store.containsKey(profile.getId())) return false;
-        store.put(profile.getId(), profile);
+        if (!store.containsKey(profile.getIdProfile())) return false;
+        store.put(profile.getIdProfile(), profile);
         return true;
     }
 
+    public void saveProductsForUser(int id, List<Product> products) {
+        productsStore.put(id, products);
+    }
 
-/*
+    public void saveUser(User user) {
+        if (user != null) {
+            usersStore.put(user.getUserId(), user);
+        }
+    }
+
+    public User findUserById(int userId) {
+        return usersStore.get(userId);
+    }
 
     @Override
-    public void saveProductForUser(int i, Product product1) {
-
+    public List<Product> findProductsByUserId(int userId) {
+        return productsStore.getOrDefault(userId, List.of());
     }
-
-    public void saveProductsForUser(int userId, List<Product> products) {
-        productsStore.put(userId, products);
-    }
-    */
 }

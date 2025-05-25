3d9d4246ff6be310a298d6d3da4de561ebb16bfe
Camila SÃanchez <domenica.sanchez@epn.edu.ec>
2025-05-25 15:57:55 -0500

    Feat(Camila): Implements ProductService - SearchResult

--- src/main/java/model/service/ProductService.java
+++ src/main/java/model/service/ProductService.java
@@ -1,15 +1,23 @@
 package model.service;
 
 import model.dao.ProductDAO;
+import model.dto.SearchResult;
 import model.entities.Product;
+import model.enums.ProductCategory;
+import model.utils.ProductCategoryHelper;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductService {
     private final ProductDAO productDAO;
 
     public ProductService() {
         productDAO = new ProductDAO();
+    }
+
+    public ProductService(ProductDAO productDAO) {
+        this.productDAO = productDAO;
     }
 
     public List<Product> findProductsByUserId(int userId) {
@@ -48,4 +56,29 @@ public class ProductService {
     public Product findById(int idProduct) {
         return productDAO.findById(idProduct);
     }
+
+
+    public SearchResult searchProductsByCategory(Object inputCategory) {
+        List<Product> products;
+        String message = null;
+
+        if (ProductCategoryHelper.isAllOrNull(inputCategory)) {
+            products = productDAO.findAll();
+        } else {
+            Optional<ProductCategory> categoryOpt = ProductCategoryHelper.parseCategory(String.valueOf(inputCategory));
+            if (categoryOpt.isPresent()) {
+                products = productDAO.getProductsByCategory(categoryOpt.get());
+                if (products.isEmpty()) {
+                    message = "There are no products in this category";
+                }
+            } else {
+                products = productDAO.findAll();
+                if (inputCategory != null && !inputCategory.toString().equalsIgnoreCase("all")) {
+                    message = "Invalid status, showing all products";
+                }
+            }
+        }
+        return new SearchResult(products, message);
+    }
+
 }
